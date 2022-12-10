c7ec281b55a3d6303db17e5e6dd601ea48bbd86b
Marcelo Vogt <marcelovogt43@gmail.com>
2022-12-10 14:22:05 +0100

    Static NavBar finished

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,25 @@
 import styles from './NavBar.module.css';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
-    return <><nav className={styles.navbar}>
-        <Link href="/"><span>Marcelo Vogt</span></Link>
-        <div className={styles.space}></div>
-        <Link href="/education"><span>Education</span></Link>
-        <Link href="/contact"><span>Contact</span></Link>
-    </nav>
-    <div className={styles.spacer}></div>
+    const router = useRouter();
+
+    return <>
+        <nav className={styles.navbar}>
+            <Link href="/" className={router.asPath == '/' ? styles.active : null}>
+                <span className={styles.icon}>home</span>
+                <span className={styles.legend}>Home</span>
+            </Link>
+            <Link href="/education" className={router.asPath == '/education' ? styles.active : null}>
+                <span className={styles.icon}>menu_book</span>
+                <span className={styles.legend}>Education</span>
+            </Link>
+            <Link href="/contact" className={router.asPath == '/contact' ? styles.active : null}>
+                <span className={styles.icon}>call</span>
+                <span className={styles.legend}>Contact</span>
+            </Link>
+        </nav>
+        <div className={styles.spacer}></div>
     </>
 }
---
+++ pages/education.js
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar";
+
+
+export default function Education() {
+    return <>
+        <h1>Education</h1>
+        <NavBar />
+    </>
+}
